b136662bdb5c93b0c7210bf3fe2834a7ca0baee4
Leonardo-Silva-Oliveira <leonardo.oliveira611@etec.sp.gov.br>
2021-08-02 17:38:22 -0300

    Initial commit

---
+++ js/desenhos.js
@@ -0,0 +1,91 @@
+var c = document.getElementById("desenho");
+
+var ctx = c.getContext("2d");
+
+ctx.moveTo(615, 25);
+
+ctx.lineTo(600, 10);
+
+ctx.lineTo(300, 360);
+
+ctx.lineTo(900, 360);
+
+ctx.lineTo(600, 25);
+
+ctx.lineTo(325, 350);
+
+ctx.lineTo(875, 350);
+
+ctx.lineTo(600, 40);
+
+ctx.lineTo(350, 340);
+
+ctx.lineTo(850, 340);
+
+ctx.lineTo(600, 55);
+
+ctx.lineTo(375, 330);
+
+ctx.lineTo(825, 330);
+
+ctx.lineTo(600, 70);
+
+ctx.lineTo(400, 320);
+
+ctx.lineTo(800, 320);
+
+ctx.lineTo(600, 85);
+
+ctx.lineTo(425, 310);
+
+ctx.lineTo(775, 310);
+
+ctx.lineTo(600, 100);
+
+ctx.lineTo(450, 300);
+
+ctx.lineTo(750, 300);
+
+ctx.lineTo(600, 115);
+
+ctx.lineTo(475, 290);
+
+ctx.lineTo(725, 290);
+
+ctx.lineTo(600, 130);
+
+ctx.lineTo(500, 280);
+
+ctx.lineTo(700, 280);
+
+ctx.lineTo(600, 145);
+
+ctx.lineTo(525, 270);
+
+ctx.lineTo(675, 270);
+
+ctx.lineTo(600, 160);
+
+ctx.lineTo(550, 260);
+
+ctx.lineTo(650, 260);
+
+ctx.lineTo(600, 175);
+
+ctx.lineTo(575, 250);
+
+ctx.lineTo(625, 250);
+
+ctx.lineTo(600, 190);
+
+ctx.lineTo(590, 240);
+
+ctx.lineTo(600, 240)
+
+ctx.strokeStyle = "red";
+
+ctx.stroke();
+
+ctx.font = "32px Times New Roman";
+
+ctx.fillText("Linhas formando um triângulo", 400, 500);
