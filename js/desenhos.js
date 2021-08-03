d47accb15fb3151cd1bed4700bcb43e48850bff6
Leonardo-Silva-Oliveira <leonardo.oliveira611@etec.sp.gov.br>
2021-08-03 10:20:48 -0300

    Implementação de novos recursos
    Adicionei mais recursos, organizei as formas criadas com as linhas, mudei as cores.

--- js/desenhos.js
+++ js/desenhos.js
@@ -2,41 +2,91 @@ var c = document.getElementById("desenho");
 
 var ctx = c.getContext("2d");
 //Posição inicial da linha.
-ctx.moveTo(200, 100);
+curva1();
+curva2();
+curva3();
+curva4();
+triangulo();
+circulo();
+quadrado();
+retangulo();
+imagem();
+
+
+function triangulo(){
+ctx.moveTo(750, 100);
 //Posição em que a linha irá.
-ctx.lineTo(100, 250);
-
-ctx.lineTo(300, 250);
-
-ctx.lineTo(200, 100);
-//Definição da cor do texto.
-ctx.fillStyle = "green";
-
-ctx.moveTo(500, 170);
-//Criação do circulo.
-ctx.arc(500, 170, 75, 0, 2 * Math.PI);
-
-ctx.moveTo(700, 100);
-
-ctx.lineTo(850, 100);
+ctx.lineTo(650, 250);
 
 ctx.lineTo(850, 250);
 
-ctx.lineTo(700, 250);
+ctx.lineTo(750, 100);
+}
+function circulo(){
+ctx.moveTo(1000, 175);
+//Criação do circulo.
+ctx.arc(1000, 175, 75, 0, 2 * Math.PI);
+}
+function quadrado(){
+ctx.moveTo(400, 100);
 
-ctx.moveTo(950, 150);
+ctx.lineTo(550, 100);
 
-ctx.lineTo(1100, 150);
+ctx.lineTo(550, 250);
 
-ctx.lineTo(1100, 250);
+ctx.lineTo(400, 250);
 
-ctx.lineTo(950, 250);
+ctx.lineTo(400, 100);
+}
+function retangulo(){
+ctx.moveTo(100, 150);
 
-ctx.lineTo(950, 150);
+ctx.lineTo(250, 150);
+
+ctx.lineTo(250, 250);
+
+ctx.lineTo(100, 250);
+
+ctx.lineTo(100, 150);
+}
+function curva1(){
+
+ctx.moveTo(430, 350);
+ctx.quadraticCurveTo(450, 280, 475, 250);
+}
+function curva2(){
+	ctx.moveTo(390, 530);
+	ctx.quadraticCurveTo(200, 400, 175, 250);
+}
+function curva3(){
+	ctx.moveTo(720, 350);
+	ctx.quadraticCurveTo(770, 300, 750, 250);
+}
+function curva4(){
+	ctx.moveTo(810, 530);
+	ctx.quadraticCurveTo(900, 420, 1000, 250);
+}
+//Criação de um gradiente para preencher as formas	
+var grd = ctx.createLinearGradient(150, 0, 750, 0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "yellow");
+//Definição da cor do texto e das bordas das formas
+ctx.strokeStyle = "SeaGreen";
 //Definição da fonte do texto.
 ctx.font = "32px Times New Roman";
 //Definição do conteúdo e posição do texto.
-ctx.fillText("Formas Geometricas", 480, 50)
+ctx.strokeText("Formas Geometricas", 480, 50)
+
+
 //Definição das cores das linhas e de seus preenchimentos.
-ctx.fillStyle = "red";
+ctx.fillStyle = grd;
+ctx.stroke();
 ctx.fill();
+
+function imagem(){
+var img = new Image();
+img.src = "Imagem1.png";
+
+img.onload = function(){
+ctx.drawImage(img, 400, 350, 400, 200);};
+}
