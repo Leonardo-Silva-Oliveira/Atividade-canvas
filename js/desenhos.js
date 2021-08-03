107db55b3ccac676a58de31dc4be99d2798ea6df
Leonardo-Silva-Oliveira <leonardo.oliveira611@etec.sp.gov.br>
2021-08-03 16:02:04 -0300

    Adição de comentários
    Adicionei comentários e arrumei as cores do site

--- js/desenhos.js
+++ js/desenhos.js
@@ -1,18 +1,22 @@
 var c = document.getElementById("desenho");
+//Caso o canvas não for compativel o site irá avisar.
 if(c.getContext){
 var ctx = c.getContext("2d");
 //Posição inicial da linha.
-curva1();
-curva2();
-curva3();
-curva4();
+
+//Chamando as funções criadas.
 triangulo();
 circulo();
 quadrado();
 retangulo();
+curva1();
+curva2();
+curva3();
+curva4();
 imagem();
+texto();
 
-
+//Funções para cada forma, curva, texto e imagens.
 function triangulo(){
 ctx.moveTo(750, 100);
 //Posição em que a linha irá.
@@ -21,11 +25,25 @@ ctx.lineTo(650, 250);
 ctx.lineTo(850, 250);
 
 ctx.lineTo(750, 100);
+
+var grd = ctx.createLinearGradient(150, 0, 750, 0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "yellow");
+
+ctx.fillStyle = grd;
+ctx.fill();
 }
 function circulo(){
 ctx.moveTo(1000, 175);
 //Criação do circulo.
 ctx.arc(1000, 175, 75, 0, 2 * Math.PI);
+
+var grd = ctx.createLinearGradient(150, 0, 750, 0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "yellow");
+
+ctx.fillStyle = grd;
+ctx.fill();
 }
 function quadrado(){
 ctx.moveTo(400, 100);
@@ -37,6 +55,13 @@ ctx.lineTo(550, 250);
 ctx.lineTo(400, 250);
 
 ctx.lineTo(400, 100);
+
+var grd = ctx.createLinearGradient(150, 0, 750, 0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "yellow");
+
+ctx.fillStyle = grd;
+ctx.fill();
 }
 function retangulo(){
 ctx.moveTo(100, 150);
@@ -48,41 +73,51 @@ ctx.lineTo(250, 250);
 ctx.lineTo(100, 250);
 
 ctx.lineTo(100, 150);
+
+var grd = ctx.createLinearGradient(150, 0, 750, 0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "yellow");
+
+ctx.fillStyle = grd;
+ctx.fill();
 }
 function curva1(){
 
 ctx.moveTo(430, 350);
 ctx.quadraticCurveTo(450, 280, 475, 250);
+
+ctx.strokeStyle = "green";
+ctx.stroke();
 }
 function curva2(){
 	ctx.moveTo(390, 530);
 	ctx.quadraticCurveTo(200, 400, 175, 250);
+	
+ctx.strokeStyle = "green";
+ctx.stroke();
 }
 function curva3(){
 	ctx.moveTo(720, 350);
 	ctx.quadraticCurveTo(770, 300, 750, 250);
+	
+ctx.strokeStyle = "green";
+ctx.stroke();
 }
 function curva4(){
 	ctx.moveTo(810, 530);
 	ctx.quadraticCurveTo(900, 420, 1000, 250);
-}
-//Criação de um gradiente para preencher as formas	
-var grd = ctx.createLinearGradient(150, 0, 750, 0);
-grd.addColorStop(0, "red");
-grd.addColorStop(1, "yellow");
-//Definição da cor do texto e das bordas das formas
-//Definição da fonte do texto.
+	
 ctx.strokeStyle = "green";
-ctx.font = "32px Times New Roman";
+ctx.stroke();
+}
+function texto(){
+	ctx.font = "32px Times New Roman";
 //Definição do conteúdo e posição do texto.
 ctx.strokeText("Formas Geometricas", 480, 50)
 
-
-//Definição das cores das linhas e de seus preenchimentos.
-ctx.fillStyle = grd;
+ctx.strokeStyle = "green";
 ctx.stroke();
-ctx.fill();
-
+}
 function imagem(){
 var img = new Image();
 img.src = "Imagem1.png";
